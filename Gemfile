45219b1fe685157863a10c3c2d42d8a72df80860
Chris Branson <138595+chrisbranson@users.noreply.github.com>
2021-08-06 17:27:56 +0100

    Add Coveralls to Github CI workflow

--- Gemfile
+++ Gemfile
@@ -5,7 +5,8 @@ group :development do
 end
 
 group :test do
-  gem 'coveralls', :require => false
+  gem 'simplecov', '~> 0.21'
+  gem 'simplecov-lcov', '~> 0.8'
 end
 
 # Specify your gem's dependencies in ruby_identicon.gemspec
--- spec/helper.rb
+++ spec/helper.rb
@@ -1,5 +1,17 @@
-require 'coveralls'
-Coveralls.wear!
+require 'simplecov'
+require 'simplecov-lcov'
+
+SimpleCov::Formatter::LcovFormatter.config do |config|
+  config.report_with_single_file = true
+  config.single_report_path = '.coverage/lcov.info'
+end
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::HTMLFormatter,
+  SimpleCov::Formatter::LcovFormatter
+])
+
+SimpleCov.start
 
 require 'rspec'
 require 'ruby_identicon'
